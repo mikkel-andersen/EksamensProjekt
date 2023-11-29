c0c88ed17b67dbf26bce08003a629ba83da29a43
mikkel <mikkel.andersen2810@gmail.com>
2023-11-29 10:52:41 +0100

    Storage metoder 29/11/2023

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,4 +1,63 @@
 package storage;
 
+import model.Destillation;
+import model.Fad;
+import model.Lager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Storage {
+    private List<Destillation> destillationer;
+    private List<Fad> fadListe;
+    private List<Lager> lager;
+
+    public Storage() {
+        destillationer = new ArrayList<>();
+        fadListe = new ArrayList<>();
+        lager = new ArrayList<>();
+    }
+
+    public List<Destillation> getDestillationer() {
+        return new ArrayList<>(destillationer);
+    }
+
+    public void addDestillation(Destillation destillation) {
+        if (!destillationer.contains(destillation)) {
+            destillationer.add(destillation);
+        }
+    }
+
+    public void removeDestillation(Destillation destillation) {
+        destillationer.remove(destillation);
+    }
+
+    public List<Fad> getFadListe() {
+        return new ArrayList<>(fadListe);
+    }
+
+    public void addFad(Fad fad) {
+        if (!fadListe.contains(fad)) {
+            fadListe.add(fad);
+        }
+    }
+
+    public void removeFad(Fad fad) {
+        fadListe.remove(fad);
+    }
+
+    public List<Lager> getLager() {
+        return new ArrayList<>(lager);
+    }
+
+    public void addLager(Lager lager) {
+        if (!this.lager.contains(lager)) {
+            this.lager.add(lager);
+        }
+    }
+
+    public void removeLager(Lager lager) {
+        this.lager.remove(lager);
+    }
+
 }
