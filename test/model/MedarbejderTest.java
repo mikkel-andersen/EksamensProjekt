d1070fb5b9199e09ba6eb28d5361ea870046fd86
mikkel <mikkel.andersen2810@gmail.com>
2023-11-29 11:44:07 +0100

    Første test af opretDestillation lavet 29/11/2023

--- test/model/MedarbejderTest.java
+++ test/model/MedarbejderTest.java
@@ -1,18 +1,27 @@
 package model;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MedarbejderTest {
 
     @org.junit.jupiter.api.Test
-    void getNavn() {
+    void opretDestillation_test_1() {
         //Arrange
+        Medarbejder medarbejder = new Medarbejder("Hans");
+        Destillation destillation = new Destillation(LocalDate.of(2023, 11,29), LocalDate.of(2026,11,23), "Fire", "Byg", 300, 10, medarbejder);
+        //Act
+        Destillation faktiskResultat = medarbejder.opretDestillation(LocalDate.of(2023, 11,29), LocalDate.of(2026,11,23), "Fire", "Byg", 300, 10);
+        //Assert
+        assertEquals(destillation.getMedarbejder(), faktiskResultat.getMedarbejder());
+        assertEquals(destillation.getAlkoholProcent(), faktiskResultat.getAlkoholProcent());
+        assertEquals(destillation.getVaeskeILiter(), faktiskResultat.getVaeskeILiter());
+        assertEquals(destillation.getKornSort(), faktiskResultat.getKornSort());
+        assertEquals(destillation.getMaltBatch(), faktiskResultat.getMaltBatch());
+        assertEquals(destillation.getSlutDato(), faktiskResultat.getSlutDato());
+        assertEquals(destillation.getStartDato(), faktiskResultat.getStartDato());
 
-        //Act & Assert
 
-    }
-
-    @org.junit.jupiter.api.Test
-    void opretDestillation() {
     }
 }
