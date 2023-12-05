805e69b3352718e7ae8e6e2476bc6bbdef6db066
BuildTools <unconfigured@null.spigotmc.org>
2023-12-05 11:50:06 +0100

    hej

--- src/gui2/LoginWindow.java
+++ src/gui2/LoginWindow.java
@@ -1,12 +1,16 @@
 package gui2;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.awt.*;
 
@@ -25,22 +29,35 @@ public class LoginWindow extends Application {
 
         Scene loginScene = new Scene(loginPane);
         stage.setScene(loginScene);
-        stage.setHeight(500);
-        stage.setWidth(500);
+        stage.setHeight(600);
+        stage.setWidth(600);
         stage.show();
     }
 
     private void initLoginContent() {
 
-        Image image = new Image() {}
+        Image image = new Image("gui2/SallLogo.png");
 
+        ImageView imageView = new ImageView(image);
 
+        imageView.fitWidthProperty();
+        imageView.fitHeightProperty();
+
+        VBox vBox = new VBox(10);
+        vBox.setAlignment(Pos.CENTER);
+
+        vBox.getChildren().addAll(imageView, createStartButton());
+
+        loginPane.setCenter(vBox);
+    }
+    private Button createStartButton(){
         Button startButton = new Button("Start");
         startButton.setMinSize(100, 70);
         startButton.setOnAction(event -> startMainApplication());
 
-        loginPane.setCenter(startButton);
+        return startButton;
     }
+
 
     private void startMainApplication() {
         StartVindue startVindue = new StartVindue();
