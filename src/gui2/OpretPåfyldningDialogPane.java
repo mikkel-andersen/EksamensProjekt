687bc7a0f08a83fdaf859ce12e97eb30874672e9
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 09:48:07 +0100

    Gui tilføjet labels

--- src/gui2/OpretPåfyldningDialogPane.java
+++ src/gui2/OpretPåfyldningDialogPane.java
@@ -6,14 +6,14 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
-import model.Lager;
-import model.Påfyldning;
+import javafx.scene.control.TextField;
+import model.*;
 import controller.Controller;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import model.Fad;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class OpretPåfyldningDialogPane extends Dialog<Påfyldning> {
 
@@ -21,6 +21,8 @@ public class OpretPåfyldningDialogPane extends Dialog<Påfyldning> {
     private ComboBox fadListeField = new ComboBox();
     private DatePicker påfyldningsDatoField = new DatePicker();
     private ComboBox lagerField = new ComboBox();
+    private ComboBox destillationField = new ComboBox();
+    private TextField mængdeField = new TextField();
     private Controller controller;
 
 
@@ -37,21 +39,34 @@ public class OpretPåfyldningDialogPane extends Dialog<Påfyldning> {
         grid.setVgap(10);
 
         fadListeField.getItems().setAll(controller.getFadListe());
-
+        destillationField.getItems().setAll(controller.getDestillationer());
         lagerField.getItems().setAll(controller.getLager());
 
         grid.add(new Label("Fad:"), 0, 0);
         grid.add(new Label("PåfyldningsDato:"), 0, 1);
         grid.add(new Label("Lager:"), 0, 2);
+        grid.add(new Label ("Destillation:"), 0, 3);
+        grid.add(new Label ("Mængde i liter"), 0, 4);
         grid.add(fadListeField, 1, 0);
         grid.add(påfyldningsDatoField, 1, 1);
         grid.add(lagerField, 1, 2);
+        grid.add(destillationField, 1, 3);
+        grid.add(mængdeField, 1, 4);
 
         this.getDialogPane().setContent(grid);
 
         this.setResultConverter((ButtonType button) -> {
             if (button == ButtonType.OK) {
-                return controller.opretPåfyldning(påfyldningsDatoField.getValue(),(Fad) fadListeField.getSelectionModel().getSelectedItem(), (model.Lager) lagerField.getSelectionModel().getSelectedItem(), null);
+
+                double mængdeValue = Double.parseDouble(mængdeField.getText());
+
+                Destillation destillation = (Destillation) destillationField.getSelectionModel().getSelectedItem();
+
+                ArrayList<Mængde> mængdeListe = new ArrayList<>();
+                mængdeListe.add(new Mængde(mængdeValue, destillation));
+
+                return controller.opretPåfyldning(påfyldningsDatoField.getValue(),
+                (Fad) fadListeField.getSelectionModel().getSelectedItem(), (model.Lager) lagerField.getSelectionModel().getSelectedItem(), mængdeListe);
             }
             return null;
         });
